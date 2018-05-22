d00da28ef23a79ae6bb543010800f510dd61e79a
Manny <Manny.Qiao@hotmail.com>
2018-05-22 23:53:38 +0800

    [Joey-H]Working on create merchant and user sign

--- src/Orchard.Web/Modules/Enjoy.Core/Models/DataDescriptor.cs
+++ src/Orchard.Web/Modules/Enjoy.Core/Models/DataDescriptor.cs
@@ -8,14 +8,44 @@ namespace Enjoy.Core.Models
     using System.Linq;
     public abstract class DataDescriptor<T> : IDataSourceDescriptor<T>
     {
+        protected DataDescriptor() { }
         public DataDescriptor(IEnumerable<T> records)
         {
-            if (IsEmptyOrNullDataSource())
+            this.Records = records;
+            if (this.IsEmptyOrNullDataSource() == false)
             {
-                this.ErrorCode = EnjoyConstant.EmptyOrNullDataSource;
+                this.ErrorCode = EnjoyConstant.Success;
+                this.ErrorMessage = EnjoyConstant.ErrorrCodeDescriptor[this.ErrorCode];
             }
         }
-        public int ErrorCode { get; set; }
+        public DataDescriptor(int errorCode, string errorMessage, IEnumerable<T> records)
+        {
+            this.ErrorCode = errorCode;
+            this.ErrorMessage = ErrorMessage;
+            this.Records = records;
+        }
+        public DataDescriptor(int errorCode)
+            : this(errorCode, EnjoyConstant.ErrorrCodeDescriptor[errorCode], Enumerable.Empty<T>())
+        {
+
+        }
+        public DataDescriptor(int errorCode, T record)
+            : this(errorCode, EnjoyConstant.ErrorrCodeDescriptor[errorCode], new List<T>() { record })
+        {
+
+        }
+        public DataDescriptor(int errorCode, string errorMessage)
+            : this(errorCode, errorMessage, Enumerable.Empty<T>())
+        {
+
+        }
+        public DataDescriptor(int errorCode, IEnumerable<T> records)
+            : this(errorCode, EnjoyConstant.ErrorrCodeDescriptor[errorCode], records)
+        {
+
+        }
+
+        public int ErrorCode { get; protected set; }
 
         public string ErrorMessage
         {
@@ -25,8 +55,22 @@ namespace Enjoy.Core.Models
 
         public abstract IEnumerable<T> Records { get; protected set; }
 
-        public abstract T GetSigleOrDefault();
-        public abstract T GetSigleOrDefault(Func<T, bool> selector);
+        public virtual T GetSigleOrDefault()
+        {
+            if (IsEmptyOrNullDataSource() == false)
+            {
+                return this.Records.FirstOrDefault();
+            }
+            return default(T);
+        }
+        public virtual T GetSigleOrDefault(Func<T, bool> selector)
+        {
+            if (IsEmptyOrNullDataSource() == false)
+            {
+                return this.Records.FirstOrDefault(selector);
+            }
+            return default(T);
+        }
         protected virtual bool IsEmptyOrNullDataSource()
         {
             return Records == null || Records.Count() == 0;
