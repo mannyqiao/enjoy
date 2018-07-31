fbbcdf6795284f74ebce8ff3b3decb887a0868ea
Manny <Manny.Qiao@hotmail.com>
2018-07-31 22:10:53 +0800

    [Joey-O]修改API参数，用于正确解密微信服务器推送的消息

--- src/Orchard.Web/Modules/Enjoy.Core/Api/EnjoyController.cs
+++ src/Orchard.Web/Modules/Enjoy.Core/Api/EnjoyController.cs
@@ -17,20 +17,20 @@ namespace Enjoy.Core.Api
         private readonly IMerchantService Merchant;
         private readonly IOrchardServices OS;
         private readonly IWeChatApi WeChat;
-        private readonly IWeChatEventBehavior Behavior;
+        private readonly IWeChatMsgHandler _handler;
         public EnjoyController(
             IOrchardServices os,
             IEnjoyAuthService auth,
             IWeChatApi wechat,
             IMerchantService merchant,
-            IWeChatEventBehavior behavior)
+            IWeChatMsgHandler handler)
         {
             this.Auth = auth;
             this.OS = os;
             this.Merchant = merchant;
             this.WeChat = wechat;
             this.Logger = NullLogger.Instance;
-            this.Behavior = behavior;
+            this._handler = handler;
         }
         public ILogger Logger { get; set; }
         [HttpGet]
@@ -64,24 +64,43 @@ namespace Enjoy.Core.Api
         ///timestamp 时间戳
         ///nonce 随机数
         ///echostr 随机字符串
+        // 微信POST 示例 api/enjoy/WxBizMsg?signature=42960d8b94eeac9c28f01bfada5b36f8afb86538
+        //&timestamp=1533011632
+        //&nonce=117779779
+        //&openid=o8ntH0r7gF7MRjz0-m8JpuFhpmEw
+        //&encrypt_type=aes
+        //&msg_signature=31afefd9f0302c1395ae23ba5d68a7de8562c369
         /// </remarks>
         [Route("api/enjoy/WxBizMsg")]
         [HttpPost]
         [HttpGet]
-        public void WxBizMsg(string signature = null, string timestamp = null, string nonce = null, string echostr = null)
+        public void WxBizMsg(
+            string signature = null,
+            string timestamp = null,
+            string nonce = null,            
+            string openid = null,
+            string encrypt_type = null,
+            string msg_signature = null,
+            string echostr = null)
         {
+
             if (this.OS.WorkContext.HttpContext.Request.HttpMethod.Equals("GET", System.StringComparison.OrdinalIgnoreCase))
             {
                 this.OS.WorkContext.HttpContext.Response.Write(echostr);
                 this.OS.WorkContext.HttpContext.Response.End();
                 return;
             }
-            var token = new WxMsgToken(signature,
+            var token = new WxMsgToken(msg_signature,
                 timestamp,
                 nonce,
                 ReadStream2String(this.OS.WorkContext.HttpContext.Request.InputStream));
-            Logger.Error(token.ToJson());
-            this.Behavior.Execute(token);
+            string weChatMsg = string.Concat(
+                string.Format("requestUrl:{0}\r\n", this.OS.WorkContext.HttpContext.Request.RawUrl),
+                string.Format("xmlbody = {0} \r\n", token.ReqMsg)
+            );
+            Logger.Error(weChatMsg);
+            this._handler.Handle(token);
+
         }
         private string ReadStream2String(Stream stream)
         {
